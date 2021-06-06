e639b3d838675d7f424fedf1047748fdfd3afe37
José Garcia Vera <70446683+Berenduin@users.noreply.github.com>
2021-06-06 20:32:30 +0200

    Create aboutUsLink.py
    El enlace de quiénes somos sueles estar normalmente bajo la ruta "/about-us" o cosas así, pero no siempre es así. 
    Aquí dejo una posible solución que utilizo a día de hoy para buscar ese codiciado enlace para varios idiomas europeos.

---
+++ aboutUsLink.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+
+def Combinations(items):
+  
+    items += [i.replace(" ", "-") for i in items if " " in i]     # blanks by -
+    items += [i.replace(" ", "_") for i in items if " " in i]     # blanks by _
+    items += [i.replace("-", "_") for i in items if "-" in i]     # - by _
+    items += [i.replace("_", "-") for i in items if "_" in i]     # vice versa
+    
+    items = sorted(set(items))
+    
+    return items
+
+
+def aboutUsLink(url,html,language):
+    
+    links = webLinks(url,html)
+    
+    dic = {'es': ['sobre nosotros', 'quienes somos', 'sobre '],
+           'fr': ['qui sommes nous','a propos de nous', 'propos de nous', 'nous '],
+           'it': ['chi siamo', 'siamo '],
+           'de': ['über uns', 'uber uns','uber '],
+           'pt': ['sobre nos', 'quem somos', 'sobre '],
+           'nl': ['over ons','over '],
+           'en': ['about us', 'about ','who is', 'who are', 'who '],
+           'pl': ['o nas', 'nas '],
+           'cs': ['o nás', 'kdo je', 'kdo jsou', 'nás ', 'kdo '],
+           'ro': ['despre noi', 'cine este', 'care sunt', 'noi ']}
+    
+    if language == 'en':
+        keywords = dic['en']
+    else:
+        keywords = dic[language] + dic['en']
+    
+    # all the possibilities
+    keywords = Combinations(keywords)
+    
+    contact_links = [[link for item in keywords] for link in links if item in link]
+    contact_links = sorted(set(contact_links))
+    
+    return contact_links
